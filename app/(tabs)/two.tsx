eac7bbfb7720af6094b9f2747b66cf95fcf6e662
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-09-10 01:11:15 -0300

    feat: added ScrollView to App

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,10 +1,31 @@
-import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import {
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { Text, View } from '@/components/Themed';
 import Participant from '@/components/Participant';
 
 export default function TabTwoScreen() {
+  const participantes = [
+    'Gabriel',
+    'Vini',
+    'Diego',
+    'Biro',
+    'Ana',
+    'Isa',
+    'Jack',
+    'Mayk',
+    'João',
+    'Felipe',
+    'Caio',
+    'Rodrigo',
+    'Amanda',
+    'Lucas',
+    'Julia',
+  ];
+
   function handleParticipantAdd() {
     console.log('Você clicou no botão de Adicionar!');
   }
@@ -13,7 +34,7 @@ export default function TabTwoScreen() {
     console.log(`Você clicou em remover o participante ${name}`);
   }
   return (
-    <View className="bg-red-500 p-4">
+    <View className="flex-1 bg-red-500 p-4">
       <Text className="text-xl font-bold text-white">Nome do evento</Text>
       <Text className="font-thin text-gray-400">
         Sexta, 4 de Novembro de 2025.
@@ -22,7 +43,6 @@ export default function TabTwoScreen() {
         <TextInput
           className="mr-3 h-14 flex-1 rounded-md bg-[#1F1E25] p-4 text-xl text-white placeholder:text-gray-400"
           placeholder="Nome do participante"
-          keyboardType="numeric"
         />
 
         <TouchableOpacity
@@ -32,28 +52,15 @@ export default function TabTwoScreen() {
           <Text className="text-2xl text-white dark:text-white">+</Text>
         </TouchableOpacity>
       </View>
-
-      <Participant
-        name="Gabriel"
-        onRemove={() => handleParticipantRemove('Gabriel')}
-      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participantes.map(participant => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipantRemove(participant)}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
--- components/Participant/index.tsx
+++ components/Participant/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Participant({ name, onRemove }: Props) {
   return (
-    <View className="mb-3 w-full flex-row items-center rounded-md bg-[#1F1E25]">
+    <View className="mb-2 w-full flex-row items-center rounded-md bg-[#1F1E25]">
       <Text className="ml-4 flex-1 text-base text-white">{name}</Text>
       <TouchableOpacity
         className="h-14 w-14 items-center justify-center rounded-md bg-red-500"
