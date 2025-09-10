8047195c8f63d71cac69b269cd83f9a338a40f50
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-09-10 00:10:48 -0300

    feat: added new button with TouchableOpacity and onPress, and another function handleParticipantAdd

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,9 +1,13 @@
-import { StyleSheet, TextInput } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
 export default function TabTwoScreen() {
+  function handleParticipantAdd() {
+    console.log('Você clicou no botão de Adicionar!');
+  }
+
   return (
     <View className="bg-red-500 p-4">
       <Text className="text-xl font-bold text-white">Nome do evento</Text>
@@ -16,6 +20,13 @@ export default function TabTwoScreen() {
         placeholder="Nome do participante"
         keyboardType="numeric"
       />
+
+      <TouchableOpacity
+        className="h-14 w-14 items-center justify-center rounded-md bg-green-500"
+        onPress={handleParticipantAdd}
+      >
+        <Text className="text-2xl text-white dark:text-white">+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
